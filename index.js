0df3f2edfb23bceef7bfab5c1c8ab1b979ed3457
asher scott <asher.scott6@gmail.com>
2022-02-23 10:26:09 -0600

    commit tree a181f27389aad8f911a6bda61201259a692c65d2

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+    let distance;
+    42 >= num 
+        ? distance = 42 - num
+        : distance = num - 42;
+    return distance;
+}
+
+function distanceFromHqInFeet(num) {
+    return distanceFromHqInBlocks(num) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    let distance;
+    start >= end 
+        ? distance = start - end
+        : distance = end - start;
+    return distance * 264;
+}
+
+function calculatesFarePrice(start, end) {
+    let distance =  distanceTravelledInFeet(start, end);
+
+    if(distance <= 400) {
+        return 0;
+    }else if(distance < 2000) {
+        return 0.02 * (distance - 400);
+    }else if(distance < 2500) {
+        return 25;
+    }else {
+        return 'cannot travel that far';
+    }
+}
